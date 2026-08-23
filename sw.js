a60198ddc45e8f59c647ecd83c7b28eb6b2c34a0
sociosofia <luizvjj@gmail.com>
2026-08-23 19:10:24 -0300

    Atualiza cache offline para fragmentos canônicos

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE = 'ifsuldeminas-simulados-v1';
+const CACHE = 'ifsuldeminas-simulados-v2';
 const CORE = [
   './', './index.html', './css/app.css', './js/app.js',
   './js/engine.part01.txt', './js/engine.part02.txt', './js/engine.part03.txt',
   './manifest.webmanifest', './assets/icon.svg',
   './assets/ifc_2026_q03_charge.part01.b64', './assets/ifc_2026_q03_charge.part02.b64',
   './data/manifest.json',
-  './data/banco.part01.b64', './data/banco.part02.b64', './data/banco.part03.b64', './data/banco.part04.b64',
-  './data/banco.part05.b64', './data/banco.part06.b64', './data/banco.part07.b64', './data/banco.part08.b64'
+  './data/banco.part01.b64', './data/banco.part02.b64',
+  './data/banco.part03a.b64', './data/banco.part03b.b64', './data/banco.part03c.b64', './data/banco.part03d.b64',
+  './data/banco.part04.b64',
+  './data/banco.part05a.b64', './data/banco.part05b.b64', './data/banco.part05c.b64', './data/banco.part05d.b64',
+  './data/banco.part06a.b64', './data/banco.part06b.b64',
+  './data/banco.part06c1a.b64', './data/banco.part06c1b1.b64', './data/banco.part06c1b2.b64',
+  './data/banco.part06c1b3a.b64', './data/banco.part06c1b3b.b64',
+  './data/banco.part06c1b3c1.b64', './data/banco.part06c1b3c2.b64', './data/banco.part06c1b3c3.b64',
+  './data/banco.part06c1b4.b64', './data/banco.part06c2.b64', './data/banco.part06d.b64',
+  './data/banco.part07.b64', './data/banco.part08.b64'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
